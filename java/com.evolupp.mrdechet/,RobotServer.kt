55cdeb72e16bea61bbf9c8fa80c9d8b56ccd4908
c37gp <colingp37@gmail.com>
2026-06-29 17:54:07 +0200

    Implement RobotServer WebSocket server

---
+++ java/com.evolupp.mrdechet/,RobotServer.kt
@@ -0,0 +1,48 @@
+package com.robot.control
+
+import org.java_websocket.server.WebSocketServer
+import org.java_websocket.WebSocket
+import org.java_websocket.handshake.ClientHandshake
+import java.net.InetSocketAddress
+
+class RobotServer(
+    port: Int,
+    private val bleSender: BleSender
+) : WebSocketServer(InetSocketAddress(port)) {
+
+    private var autoMode = false
+
+    override fun onOpen(conn: WebSocket?, handshake: ClientHandshake?) {}
+
+    override fun onMessage(conn: WebSocket?, message: String?) {
+        message ?: return
+
+        when (message) {
+
+            "auto_on" -> autoMode = true
+            "auto_off" -> autoMode = false
+
+            "avant",
+            "arriere",
+            "gauche",
+            "droite",
+            "stop",
+            "dechet" -> {
+
+                if (!autoMode) {
+                    bleSender.send(message)
+                }
+            }
+        }
+    }
+
+    override fun onClose(conn: WebSocket?, code: Int, reason: String?, remote: Boolean) {}
+
+    override fun onError(conn: WebSocket?, ex: Exception?) {
+        ex?.printStackTrace()
+    }
+
+    override fun onStart() {
+        println("WebSocket serveur démarré")
+    }
+}
